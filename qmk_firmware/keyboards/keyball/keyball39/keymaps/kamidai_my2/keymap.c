3bf0a1b73151e31e8bdb9cbe93eeddfbc6385f5a
Aress1988 <s_ryo_tarou1_8@yahoo.co.jp>
2024-06-29 21:12:08 +0900

    240629 FWのベースを変更 v1.0.3

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_my2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_my2/keymap.c
@@ -116,7 +116,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   // レイヤーが1または3の場合、スクロールモードが有効になる
   keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
   // keyball_set_scroll_mode(get_highest_layer(state) == 1);
-
+  
+  /*
   // レイヤーとLEDを連動させる
   switch (get_highest_layer(state)) {
     case click_layer:
@@ -135,7 +136,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     default:
       rgblight_sethsv(HSV_OFF);
   }
-
+  */
   return state;
 }
 
